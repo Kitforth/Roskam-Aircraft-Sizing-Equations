18f4bb6e8ed8b3431ce8937a7cc90cbf6abaf18d
Kitforth <116692287+Kitforth@users.noreply.github.com>
2024-03-19 15:16:11 +1030

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+class Aircraft():
+    self.table_of_coefficients = {}
+
+    def wing(self):
+        _Chord
+        _Wing_length
+        _S = _Chord * Wing_length * 2
+        _A
+        _Aerofoil
+        pass
+
+    def fuselage(self):
+        Weight_fuselage
+        Length_fuselage
+        Width_fuselage
+        pass
+
+    def empennage(self):
+        pass
+
+    def landing_gear(self):
+        Weight_landing_gear
+        pass
+
+    def engine(self):
+        Weight_engine
+        Thrust
+        pass
+
+    def payload(self):
+        Weight_Payload
+        pass
+
+class wing():
