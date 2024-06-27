6af63cea37ee090dae96d0f8a9435aa906ea518b
Kitforth <116692287+Kitforth@users.noreply.github.com>
2024-06-27 16:31:37 +0930

    Update Rotor_sizing.py
    Added classes for rotors and VTOL drone for hover and lift calculations.
    
    Idea is that as you calculate each parameter, they record the calculated parameter to the drone object you've created. and eventually you can just output the object parameters to give you your design specs.

--- Rotor_sizing.py
+++ Rotor_sizing.py
@@ -1,31 +1,46 @@
 import math
 ## Rotor sizing equation. for a given thrust find rotor size
 
+
+class Rotor:
+    def __init__(self, radius, pitch, num_of_blades, Prop_efficiency = None):
+        self.radius = radius
+        self.pitch = pitch
+        self.blades = num_of_blades
+        self.Prop_area = math.pi*self.radius**2
+
+
+class VTOL_Drone:
+    def __init__(self, weight, number_of_rotors):
+        self.weight = weight
+        self.num_rotors = number_of_rotors
+
+
 def VTOL_calc(weight, num_rotors, accel_v):
-    hover_thrust = weight/num_rotors*9.8
+    hover_thrust = weight/num_rotors * 9.8
     accel_thrust = (accel_v * weight)/num_rotors + hover_thrust
     return accel_thrust
 
-def size_rotor(Thrust, Prop_area, Prop_efficiency, rho=1.204):
+def engine_power(Thrust, Prop_area, Prop_efficiency, rho = 1.204):
 
     # Wikipedia, stationary open rotor equation for thrust
     Power = math.sqrt((Thrust**3)(2*rho*Prop_area))
+    Power_engine = Power/Prop_efficiency
+    return Power_engine
 
-    P_engine=Power/Prop_efficiency
 
-
-def Rotor_speed_check(rpm, R, speed_of_sound=343):
+def Rotor_speed_check(rpm, R, speed_of_sound = 343):
     # R = radius of rotor in metres
-    tip_speed=rpm*60*2*math.pi*R
+    tip_speed=rpm * 60 * 2 * math.pi*R
     if tip_speed >= speed_of_sound:
         # This should really be an exception.
         print("tip speed of rotor exceeds speed of sound. Speed is: ", tip_speed)
 
-        max_size_rotor = speed_of_sound/(rpm*60*2*math.pi)
+        max_size_rotor = speed_of_sound/(rpm * 60 * 2 * math.pi)
         print("Rotor must be smaller than:", max_size_rotor)
 
-        max_rotor_rpm = speed_of_sound/(60*2*math.pi*R)
-        print("Alternatively, RPM must be below:",max_rotor_rpm)
+        max_rotor_rpm = speed_of_sound/(60 * 2 * math.pi * R)
+        print("Alternatively, RPM must be below:", max_rotor_rpm)
     else:
         print("Rotor max speed is:", tip_speed)
 
